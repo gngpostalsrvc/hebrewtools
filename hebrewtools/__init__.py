aeca1e541b1fe8516d14c0f518b9a9954baf2d42
Aren Wilson-Wright <wilsonwright@utexas.edu>
2022-11-30 14:17:38 -0600

    Add files via upload

--- hebrewtools/__init__.py
+++ hebrewtools/__init__.py
@@ -1,7 +1,1 @@
-from . import sbl_normalization
-from . import is_sbl_normalized
 
-__all__ = [
-    'sbl_normalization',
-    'is_sbl_normalized'
-]
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('HISTORY.md') as history_file:
 
 setup_args = dict(
     name='hebrewtools',
-    version='0.0.1',
+    version='0.1.0',
     description='Useful tools for working with vocalized Hebrew text in Python',
     long_description_content_type="text/markdown",
     long_description=README + '\n\n' + HISTORY,
@@ -16,7 +16,7 @@ setup_args = dict(
     packages=find_packages(),
     author='Aren Wilson-Wright',
     author_email='wilsonwright.a@gmail.com',
-    keywords=['Biblical Hebrew', 'vocalization', 'normalization],
+    keywords=['Biblical Hebrew', 'vocalization', 'normalization'],
     url='https://github.com/gngpostalsrvc/hebrewtools',
     download_url='https://pypi.org/project/hebrewtools/'
 )
@@ -24,4 +24,4 @@ setup_args = dict(
 install_requires = []
 
 if __name__ == '__main__':
-    setup(**setup_args, install_requires=install_requires)
+    setup(**setup_args, install_requires=install_requires, setup_requires=['wheel'])
